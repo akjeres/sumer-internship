10c9644ee6cd083d41c355e67ccd7f00cc2389d1
akjeres <akjeres@gmail.com>
2019-06-24 22:23:39 +0300

    Build added

---
+++ Level3_Framework/build/precache-manifest.d66d962ff1a29daa74d13e3b1ccdaafa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc2fdf743f0f0c01924e40b46ad688e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a516392bdddf4a759c7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "367d627c9ca74515a519",
+    "url": "/static/js/2.b9839dee.chunk.js"
+  },
+  {
+    "revision": "6a516392bdddf4a759c7",
+    "url": "/static/js/main.9b65d92f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de492fd61e0c836d4a159fd29a1f56ea",
+    "url": "/static/media/hotdog.de492fd6.svg"
+  },
+  {
+    "revision": "e8c3b34635c1bf61e0220267eb261d05",
+    "url": "/static/media/instagram.e8c3b346.svg"
+  }
+]);
