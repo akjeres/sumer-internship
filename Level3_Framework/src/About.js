6b33b8526686e3dced45dc8c4abc805795bf38bf
akjeres <akjeres@gmail.com>
2019-06-25 19:29:37 +0300

    Components rewrited using Classes

--- Level3_Framework/src/About.js
+++ Level3_Framework/src/About.js
@@ -1,22 +1,30 @@
 import React from "react";
 import hot_dog from './img/hotdog.svg';
 
-function About() {
-    function comingSoon() {
-        alert('Coming soon');
+class About extends React.Component {
+    constructor() {
+        super()
     }
-    document.title = 'About Us | Dirty Dogs';
-    return (
-        <section id="about" className="main_about">
-            <div className="container">
-                <div className="main_about__wrapper">
-                    <img className="about_logo" src={hot_dog} alt='Hot Dog' />
-                    <h1 className="about_description">Dirty Dogs serves all-beef, vegan and vegatagian hot&nbsp;dogs.</h1>
-                    <a onClick={comingSoon} className="about_link">More Dogs ‘n Make Em Hot</a>
+    componentDidMount() {
+        document.title = 'About Us | Dirty Dogs';
+    }
+    
+    render() {
+        function comingSoon() {
+            alert('Coming soon');
+        }
+        return (
+            <section id="about" className="main_about">
+                <div className="container">
+                    <div className="main_about__wrapper">
+                        <img className="about_logo" src={hot_dog} alt='Hot Dog' />
+                        <h1 className="about_description">Dirty Dogs serves all-beef, vegan and vegatagian hot&nbsp;dogs.</h1>
+                        <a onClick={comingSoon} className="about_link">More Dogs ‘n Make Em Hot</a>
+                    </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    }
   }
 
 
